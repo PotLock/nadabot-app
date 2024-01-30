e57e1b7481f3559430c14e02029c7bcad877769e
Wenderson Pires <wendersonpdas@gmail.com>
2024-01-30 04:02:38 -0300

    environment indicator (staging and testnet only)

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -143,78 +143,101 @@ const NavBar = () => {
     walletApi.signInModal();
   }, []);
 
-  return (
-    <Stack
-      direction="row"
-      width="100%"
-      sx={{
-        borderBottomWidth: "1px",
-        borderBottomColor: colors.LIGHTGRAY,
-        borderBottomStyle: "solid",
-        mb: 3,
-      }}
-    >
-      <Container>
-        <Stack
-          direction={maxWidth430 && walletConnected ? "column" : "row"}
-          height={maxWidth430 ? "fit-content" : 96}
-          justifyContent="space-between"
-          alignItems="center"
-          my={maxWidth430 ? 2 : 0}
-        >
-          {/* Left */}
-          <ButtonContainer onClick={goHomeHandler}>
-            <NadabotLogo />
-          </ButtonContainer>
+  // Environment indicator (staging and testnet only)
+  let hostEnv = window.location.host.includes("staging")
+    ? "staging"
+    : window.location.host.includes("testnet")
+      ? "testnet"
+      : null;
 
-          {/* Right */}
-          <Stack>
-            {walletConnected ? (
-              <Stack direction="row" mb={maxWidth430 ? 2 : 0}>
-                <Stack
-                  direction={maxWidth430 ? "column" : "row"}
-                  width={isAdmin ? 350 : 300}
-                  justifyContent="space-between"
-                  alignItems="center"
-                >
-                  <BellIcon
-                    sx={{ width: 14, display: maxWidth430 ? "none" : "block" }}
-                  />
-                  <Divider
-                    orientation="vertical"
-                    sx={{
-                      bgcolor: colors.PRIMARY,
-                      display: maxWidth430 ? "none" : "block",
-                    }}
-                    flexItem
-                  />
-                  <Dropbox />
-                  <Tag
-                    sx={{ mt: maxWidth430 ? 1 : 0 }}
-                    type={isAdmin || isVerifiedHuman ? "blue" : "red"}
-                    label={
-                      isAdmin || isVerifiedHuman
-                        ? "Verified Human"
-                        : "Not A Human"
-                    }
-                  />
-                </Stack>
-              </Stack>
-            ) : (
-              <Button
-                variant="contained"
-                color="primary"
-                size="small"
-                disableRipple
-                onClick={connectWalletHandler}
-              >
-                Login
-              </Button>
-            )}
-          </Stack>
+  hostEnv = window.location.host.includes("localhost") ? "testnet" : hostEnv;
+
+  return (
+    <>
+      {/* Environment Indicator */}
+      {hostEnv && (
+        <Stack alignItems="center" mb={2} bgcolor={colors.NEUTRAL100}>
+          <Typography fontWeight={600} py={2}>
+            <strong>network:</strong> {hostEnv}
+          </Typography>
         </Stack>
-      </Container>
-    </Stack>
+      )}
+
+      <Stack
+        direction="row"
+        width="100%"
+        sx={{
+          borderBottomWidth: "1px",
+          borderBottomColor: colors.LIGHTGRAY,
+          borderBottomStyle: "solid",
+          mb: 3,
+        }}
+      >
+        <Container>
+          <Stack
+            direction={maxWidth430 && walletConnected ? "column" : "row"}
+            height={maxWidth430 ? "fit-content" : 96}
+            justifyContent="space-between"
+            alignItems="center"
+            my={maxWidth430 ? 2 : 0}
+          >
+            {/* Left */}
+            <ButtonContainer onClick={goHomeHandler}>
+              <NadabotLogo />
+            </ButtonContainer>
+
+            {/* Right */}
+            <Stack>
+              {walletConnected ? (
+                <Stack direction="row" mb={maxWidth430 ? 2 : 0}>
+                  <Stack
+                    direction={maxWidth430 ? "column" : "row"}
+                    width={isAdmin ? 350 : 300}
+                    justifyContent="space-between"
+                    alignItems="center"
+                  >
+                    <BellIcon
+                      sx={{
+                        width: 14,
+                        display: maxWidth430 ? "none" : "block",
+                      }}
+                    />
+                    <Divider
+                      orientation="vertical"
+                      sx={{
+                        bgcolor: colors.PRIMARY,
+                        display: maxWidth430 ? "none" : "block",
+                      }}
+                      flexItem
+                    />
+                    <Dropbox />
+                    <Tag
+                      sx={{ mt: maxWidth430 ? 1 : 0 }}
+                      type={isAdmin || isVerifiedHuman ? "blue" : "red"}
+                      label={
+                        isAdmin || isVerifiedHuman
+                          ? "Verified Human"
+                          : "Not A Human"
+                      }
+                    />
+                  </Stack>
+                </Stack>
+              ) : (
+                <Button
+                  variant="contained"
+                  color="primary"
+                  size="small"
+                  disableRipple
+                  onClick={connectWalletHandler}
+                >
+                  Login
+                </Button>
+              )}
+            </Stack>
+          </Stack>
+        </Container>
+      </Stack>
+    </>
   );
 };
 
