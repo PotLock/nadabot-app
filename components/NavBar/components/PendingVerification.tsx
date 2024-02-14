2228f1e75d20a01aa8133636b6617cf151359d1d
Wenderson Pires <wendersonpdas@gmail.com>
2024-02-14 19:04:02 -0300

    add_stamp method for Pending Verification Notification

--- components/NavBar/components/PendingVerification.tsx
+++ components/NavBar/components/PendingVerification.tsx
@@ -1,26 +1,32 @@
-import { Stack, Typography } from "@mui/material";
+import { CircularProgress, Stack, Typography } from "@mui/material";
+import { useCallback, useState } from "react";
 
 import CustomButton from "@nadabot/components/ui/CustomButton";
 import useBreakPoints from "@nadabot/hooks/useBreakPoints";
+import { add_stamp } from "@nadabot/services/contracts/sybil.nadabot";
 import { ProviderExternalWithIsHuman } from "@nadabot/services/contracts/sybil.nadabot/interfaces/providers";
 import colors from "@nadabot/theme/colors";
 import { BellIcon } from "@nadabot/theme/icons";
 import truncate from "@nadabot/utils/truncate";
 
 type ItemProps = {
-  contractName: string;
-  contractId: string;
-  method: string;
+  providerInfo: ProviderExternalWithIsHuman;
   showBottomBorder?: boolean;
 };
 
-function Item({
-  contractName,
-  contractId,
-  method,
-  showBottomBorder,
-}: ItemProps) {
+function Item({ providerInfo, showBottomBorder }: ItemProps) {
   const { maxWidth430, maxWidth1144 } = useBreakPoints();
+  const [loading, isLoading] = useState(false);
+
+  const verifyHandler = useCallback(async () => {
+    isLoading(true);
+    try {
+      await add_stamp(providerInfo.provider_id);
+    } catch (error) {
+      console.error(error);
+    }
+    isLoading(false);
+  }, [providerInfo.provider_id]);
 
   return (
     <Stack
@@ -38,7 +44,7 @@ function Item({
           lineHeight="normal"
           mr={maxWidth430 ? 0 : 2}
         >
-          {maxWidth1144 ? contractName : truncate(contractName, 15)}
+          {maxWidth1144 ? providerInfo.name : truncate(providerInfo.name, 15)}
         </Typography>
         <Typography
           fontSize={10}
@@ -48,7 +54,9 @@ function Item({
           mr={maxWidth430 ? 0 : 2}
           color={colors.NEUTRAL500}
         >
-          {maxWidth1144 ? contractId : truncate(contractId, 18)}
+          {maxWidth1144
+            ? providerInfo.contract_id
+            : truncate(providerInfo.contract_id, 18)}
         </Typography>
         <Typography
           fontSize={10}
@@ -57,22 +65,34 @@ function Item({
           sx={{ textDecoration: "underline" }}
           color={colors.NEUTRAL500}
         >
-          {`${method}()`}
+          {`${providerInfo.method_name}()`}
         </Typography>
-        <CustomButton
-          sx={{
-            width: 60,
-            height: 24,
-            pt: 1.5,
-            ml: maxWidth430 ? 0 : 2,
-            fontSize: 12,
-            background: colors.PRIMARY,
-            color: colors.WHITE,
-            ":hover": { background: "#515151" },
-          }}
-        >
-          Verify
-        </CustomButton>
+        {loading ? (
+          <Stack
+            ml={maxWidth430 ? 0 : 2}
+            width="60px"
+            alignItems="center"
+            py={0.75}
+          >
+            <CircularProgress size={16} sx={{ color: colors.BLUE }} />
+          </Stack>
+        ) : (
+          <CustomButton
+            sx={{
+              width: 60,
+              height: 24,
+              pt: 1.5,
+              ml: maxWidth430 ? 0 : 2,
+              fontSize: 12,
+              background: colors.PRIMARY,
+              color: colors.WHITE,
+              ":hover": { background: "#515151" },
+            }}
+            onClick={verifyHandler}
+          >
+            Verify
+          </CustomButton>
+        )}
       </Stack>
     </Stack>
   );
@@ -111,9 +131,7 @@ export default function PendingVerification({
         {providers.slice(0, 3).map((provider, index) => (
           <Item
             key={provider.provider_id}
-            contractName={provider.name}
-            contractId={provider.contract_id}
-            method={provider.method_name}
+            providerInfo={provider}
             showBottomBorder={providers.length - 1 === index}
           />
         ))}
