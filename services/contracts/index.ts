8cc977dde24ed8b0dfcaa57c59ed37f701738198
codingshot <45281667+codingshot@users.noreply.github.com>
2024-03-24 16:02:02 -0400

    Revert "update wallet selectors"

--- services/contracts/index.ts
+++ services/contracts/index.ts
@@ -1,20 +1,9 @@
-import naxios from "@wpdas/naxios";
-import { setupBitgetWallet } from "@near-wallet-selector/bitget-wallet";
-import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
-import { setupSender } from "@near-wallet-selector/sender";
 import { setupHereWallet } from "@near-wallet-selector/here-wallet";
-import { setupMathWallet } from "@near-wallet-selector/math-wallet";
-import { setupNightly } from "@near-wallet-selector/nightly";
-import { setupMeteorWallet } from "@near-wallet-selector/meteor-wallet";
-import { setupNarwallets } from "@near-wallet-selector/narwallets";
-import { setupWelldoneWallet } from "@near-wallet-selector/welldone-wallet";
 import { setupLedger } from "@near-wallet-selector/ledger";
-import { setupNearFi } from "@near-wallet-selector/nearfi";
-import { setupCoin98Wallet } from "@near-wallet-selector/coin98-wallet";
-import { setupNeth } from "@near-wallet-selector/neth";
-import { setupXDEFI } from "@near-wallet-selector/xdefi";
+import { setupMeteorWallet } from "@near-wallet-selector/meteor-wallet";
+import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
 import { setupNearMobileWallet } from "@near-wallet-selector/near-mobile-wallet";
-import { setupMintbaseWallet } from "@near-wallet-selector/mintbase-wallet";
+import naxios from "@wpdas/naxios";
 
 import { CONTRACT_ID, NETWORK } from "@nadabot/constants";
 
@@ -23,22 +12,11 @@ export const naxiosInstance = new naxios({
   contractId: CONTRACT_ID,
   network: NETWORK,
   walletSelectorModules: [
-    setupBitgetWallet(),
     setupMyNearWallet(),
-    setupSender(),
-    setupHereWallet(),
-    setupMathWallet(),
-    setupNightly(),
-    setupMeteorWallet(),
-    setupNarwallets(),
-    setupWelldoneWallet(),
     setupLedger(),
-    setupNearFi(),
-    setupCoin98Wallet(),
-    setupNeth(),
-    setupXDEFI(),
+    setupHereWallet(),
+    setupMeteorWallet(),
     setupNearMobileWallet(),
-    setupMintbaseWallet(),
   ],
 });
 
