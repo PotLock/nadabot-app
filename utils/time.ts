1ceed00864458b2eac3f012cf24ee25d012f9d06
Carina.Akaia.io <cvo.akaia@gmail.com>
2024-05-06 15:19:32 +0400

    Handle duration string parsing error

--- utils/time.ts
+++ utils/time.ts
@@ -1,11 +1,33 @@
 import { Temporal } from "temporal-polyfill";
 
-export const millisecondsToDays = (milliseconds: number | null) =>
-  typeof milliseconds === "number"
-    ? Temporal.Duration.from({ milliseconds }).total("days")
-    : 0;
+export const millisecondsToDays = (
+  value: Temporal.DurationLike["milliseconds"] | string | null,
+) => {
+  try {
+    return Temporal.Duration.from({
+      milliseconds: typeof value === "string" ? parseInt(value) : value ?? 0,
+    }).total("days");
+  } catch {
+    const error = new TypeError(`Unable to convert \`${value}\` to days`);
 
-export const daysToMilliseconds = (days: number | null) =>
-  typeof days === "number"
-    ? Temporal.Duration.from({ days }).total("milliseconds")
-    : 0;
+    console.error(error);
+    throw error;
+  }
+};
+
+export const daysToMilliseconds = (
+  value: Temporal.DurationLike["days"] | string | null,
+) => {
+  try {
+    return Temporal.Duration.from({
+      days: typeof value === "string" ? parseInt(value) : value ?? 0,
+    }).total("milliseconds");
+  } catch {
+    const error = new TypeError(
+      `Unable to convert \`${value}\` to milliseconds`,
+    );
+
+    console.error(error);
+    throw error;
+  }
+};
