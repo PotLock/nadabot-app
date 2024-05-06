acf6a40b4116a9ead88f56c28563262ba5bce809
Carina.Akaia.io <cvo.akaia@gmail.com>
2024-05-06 15:50:10 +0400

    doc: Add JSDoc

--- utils/time.ts
+++ utils/time.ts
@@ -1,8 +1,15 @@
 import { Temporal } from "temporal-polyfill";
 
-export const millisecondsToDays = (
-  value: Temporal.DurationLike["milliseconds"] | string | null,
-) => {
+/**
+ * Converts a value in milliseconds to the equivalent number of days.
+ *
+ * @param {number | string | null} value - The value to convert,
+ *  which can be a number of milliseconds, a string representing a number of milliseconds, or null.
+ *
+ * @return {number} The number of days equivalent to the input value.
+ * @throws {TypeError} If the input value cannot be converted to a number of milliseconds.
+ */
+export const millisecondsToDays = (value: number | string | null): number => {
   try {
     return Temporal.Duration.from({
       milliseconds: typeof value === "string" ? parseInt(value) : value ?? 0,
@@ -15,9 +22,16 @@ export const millisecondsToDays = (
   }
 };
 
-export const daysToMilliseconds = (
-  value: Temporal.DurationLike["days"] | string | null,
-) => {
+/**
+ * Converts a value in days to the equivalent number of milliseconds.
+ *
+ * @param {number | string | null} value - The value to convert,
+ *  which can be a number of days, a string representing a number of days, or null.
+ *
+ * @return {number} The number of milliseconds equivalent to the input value.
+ * @throws {TypeError} If the input value cannot be converted to a number of milliseconds.
+ */
+export const daysToMilliseconds = (value: number | string | null): number => {
   try {
     return Temporal.Duration.from({
       days: typeof value === "string" ? parseInt(value) : value ?? 0,
